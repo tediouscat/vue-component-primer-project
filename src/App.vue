b3cd627f9667bbf8de5a7f4a7937eb0bc3aa8907
tediouscat <515889839@qq.com>
2024-04-05 18:37:21 +0800

    Vue项目空模板

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,8 @@
 <script setup>
-import Header from "./components/header.vue";
-import Footer from "./components/footer.vue";
-import {reactive} from "vue";
 
-const propsWeb=reactive({
-  user:10,
-  url:"www.baidu.com"
-})
-
-const userAdd=()=>{
-  propsWeb.user++;
-}
 </script>
 
 <template>
-
-  <Header propsName="tediouscat" propsUrl="www.baidu.com"/>
-  content
-  <Footer v-bind="propsWeb"/>
-  <button @click="userAdd">点击按钮</button>
 
 </template>
 
